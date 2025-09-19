89b5087e911c36f19f4e48f259b75e1ca698ba70
Jonas Ribeiro <jonas.ribeiro@rodipa.com.br>
2025-09-19 14:06:22 -0300

    my first commit

---
+++ src/test/java/rosa/ribeiro/jonas/cardapio/CardapioApplicationTests.java
@@ -0,0 +1,13 @@
+package rosa.ribeiro.jonas.cardapio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
